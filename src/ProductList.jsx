f52d80e8f373df1bf212abf08d5cb30736be4858
Saniya-3104 <31241761@vupune.ac.in>
2026-09-08 00:09:16 +0530

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -16,14 +16,16 @@ function ProductList() {
       category: "Air Purifying Plants",
       plants: [
         { name: "Snake Plant", image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg", cost: "$15" },
-        { name: "Spider Plant", image: "https://cdn.pixabay.com/photo/2018/07/11/06/47/chlorophytum-3530413_1280.jpg", cost: "$12" }
+        { name: "Spider Plant", image: "https://cdn.pixabay.com/photo/2018/07/11/06/47/chlorophytum-3530413_1280.jpg", cost: "$12" },
+        { name: "Peace Lily", image: "https://cdn.pixabay.com/photo/2019/06/12/14/14/peace-lily-4269365_1280.jpg", cost: "$18" }
       ]
     },
     {
       category: "Aromatic Plants",
       plants: [
         { name: "Lavender", image: "https://cdn.pixabay.com/photo/2017/07/18/18/24/lavender-2516623_1280.jpg", cost: "$18" },
-        { name: "Jasmine", image: "https://cdn.pixabay.com/photo/2018/01/08/17/23/jasmine-3069818_1280.jpg", cost: "$20" }
+        { name: "Jasmine", image: "https://cdn.pixabay.com/photo/2018/01/08/17/23/jasmine-3069818_1280.jpg", cost: "$20" },
+        { name: "Rosemary", image: "https://cdn.pixabay.com/photo/2019/10/11/07/12/rosemary-4541241_1280.jpg", cost: "$14" }
       ]
     }
   ];
@@ -36,8 +38,8 @@ function ProductList() {
     <div>
       <nav className="navbar">
         <h2>Paradise Nursery</h2>
-        <button onClick={() => setShowCart(!showCart)}>
-          Cart ({totalQuantity})
+        <button onClick={() => setShowCart(!showCart)} className="cart-icon-button">
+          🛒 Cart ({totalQuantity})
         </button>
       </nav>
 
