5aea1d70cba25a92c4c8f7c4bca958f82fe301bd
Saniya-3104 <31241761@vupune.ac.in>
2026-09-08 00:14:46 +0530

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -17,7 +17,10 @@ function ProductList() {
       plants: [
         { name: "Snake Plant", image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg", cost: "$15" },
         { name: "Spider Plant", image: "https://cdn.pixabay.com/photo/2018/07/11/06/47/chlorophytum-3530413_1280.jpg", cost: "$12" },
-        { name: "Peace Lily", image: "https://cdn.pixabay.com/photo/2019/06/12/14/14/peace-lily-4269365_1280.jpg", cost: "$18" }
+        { name: "Peace Lily", image: "https://cdn.pixabay.com/photo/2019/06/12/14/14/peace-lily-4269365_1280.jpg", cost: "$18" },
+        { name: "Boston Fern", image: "https://cdn.pixabay.com/photo/2020/04/30/19/52/boston-fern-5114414_1280.jpg", cost: "$14" },
+        { name: "Rubber Plant", image: "https://cdn.pixabay.com/photo/2020/02/15/11/49/plant-4850682_1280.jpg", cost: "$22" },
+        { name: "Aloe Vera", image: "https://cdn.pixabay.com/photo/2018/04/02/18/10/aloe-vera-3284643_1280.jpg", cost: "$10" }
       ]
     },
     {
@@ -25,7 +28,10 @@ function ProductList() {
       plants: [
         { name: "Lavender", image: "https://cdn.pixabay.com/photo/2017/07/18/18/24/lavender-2516623_1280.jpg", cost: "$18" },
         { name: "Jasmine", image: "https://cdn.pixabay.com/photo/2018/01/08/17/23/jasmine-3069818_1280.jpg", cost: "$20" },
-        { name: "Rosemary", image: "https://cdn.pixabay.com/photo/2019/10/11/07/12/rosemary-4541241_1280.jpg", cost: "$14" }
+        { name: "Rosemary", image: "https://cdn.pixabay.com/photo/2019/10/11/07/12/rosemary-4541241_1280.jpg", cost: "$14" },
+        { name: "Mint", image: "https://cdn.pixabay.com/photo/2016/01/26/18/08/mint-1163013_1280.jpg", cost: "$8" },
+        { name: "Eucalyptus", image: "https://cdn.pixabay.com/photo/2019/09/16/08/38/eucalyptus-4480436_1280.jpg", cost: "$16" },
+        { name: "Thyme", image: "https://cdn.pixabay.com/photo/2017/06/12/19/02/thyme-2396483_1280.jpg", cost: "$10" }
       ]
     }
   ];
