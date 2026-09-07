bdb28a0b2d589cdc2639f0bbdd8503317c06d80c
Saniya-3104 <31241761@vupune.ac.in>
2026-09-07 23:55:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,29 @@
+import React, { useState } from 'react';
+import ProductList from './ProductList';
+import './App.css';
 
+function App() {
+  const [showProductList, setShowProductList] = useState(false);
+
+  const handleGetStartedClick = () => {
+    setShowProductList(true);
+  };
+
+  return (
+    <div className="app-container">
+      {!showProductList ? (
+        <div className="background-image landing-page">
+          <h1>Welcome to Paradise Nursery</h1>
+          <p>Where Green Meets Serenity</p>
+          <button className="get-started-button" onClick={handleGetStartedClick}>
+            Get Started
+          </button>
+        </div>
+      ) : (
+        <ProductList />
+      )}
+    </div>
+  );
+}
+
+export default App;
